338eceb2963d1fe47cbeeca1e39ec9836e87d11b
diksha <65006493+diksha0799@users.noreply.github.com>
2020-10-02 22:12:25 +0530

    Update models.py

--- portfolio/projects/models.py
+++ portfolio/projects/models.py
@@ -5,6 +5,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='shop/images', default="")
     def __str__(self):
         return self.title
     
